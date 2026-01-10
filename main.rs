b1ac0dbeca12b8b8482477745ca938f501bc435d
Change-Goose-Open-Surce-Software <dimitri.andreas.hiebert@gmail.com>
2026-01-10 11:03:24 +0100

    Refactor calculation logic and drawing area initialization

--- main.rs
+++ main.rs
@@ -1,8 +1,5 @@
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, Button, Entry, ComboBoxText, Label, DrawingArea, Box as GtkBox, Orientation, ScrolledWindow};
-use webbrowser;
-use std::process::Command;
-use gtk::gdk::RGBA;
+use gtk::{Application, ApplicationWindow, Button, Entry, ComboBoxText, Label, DrawingArea, Box as GtkBox, Orientation};
 use std::rc::Rc;
 use std::cell::RefCell;
 
@@ -28,7 +25,6 @@ fn build_ui(app: &Application) {
     
     let current_lang = config.borrow().language.current.clone();
     let toolbar_position = config.borrow().ui.toolbar_position.clone();
-    let square_color = config.borrow().colors.square.clone();
 
     let window = ApplicationWindow::builder()
         .application(app)
@@ -81,24 +77,40 @@ fn build_ui(app: &Application) {
         translations.get(&current_lang, "result_default")));
 
     // Drawing Area
-    let drawing_area = DrawingArea::new();
-    drawing_area.set_size_request(300, 300);
+    let _drawing_area = DrawingArea::new();
+    _drawing_area.set_size_request(300, 300);
 
     // Calculate Logic
     let calculate_fn = {
         let input_entry = input_entry.clone();
         let unit_combo = unit_combo.clone();
         let result_label = result_label.clone();
+        let current_lang = current_lang.clone();
+        let translations = translations.clone();
         move |_: &Button| {
             let input = input_entry.text().parse::<f64>();
             if let Ok(value) = input {
-                let result = match unit_combo.active_text().as_deref() {
-                    Some("Pixel") => dpi::px_to_mm(value),
-                    Some("mm") => dpi::mm_to_px(value),
-                    Some("Inch") => dpi::px_to_inch(value),
-                    _ => value
+                let active_text = unit_combo.active_text();
+                let pixel_text = translations.get(&current_lang, "pixel");
+                let mm_text = translations.get(&current_lang, "millimeter");
+                let inch_text = translations.get(&current_lang, "inch");
+                
+                let result = if let Some(text) = active_text {
+                    let text_str = text.as_str();
+                    if text_str == pixel_text {
+                        dpi::px_to_mm(value)
+                    } else if text_str == mm_text {
+                        dpi::mm_to_px(value)
+                    } else if text_str == inch_text {
+                        dpi::px_to_inch(value)
+                    } else {
+                        value
+                    }
+                } else {
+                    value
                 };
-                result_label.set_text(&format!("Ergebnis: {:.2}", result));
+                
+                result_label.set_markup(&format!("<span size='x-large'>Ergebnis: {:.2}</span>", result));
             }
         }
     };
@@ -114,6 +126,7 @@ fn build_ui(app: &Application) {
     content_box.add(&input_entry);
     content_box.add(&unit_combo);
     content_box.add(&calculate_button);
+    content_box.add(&result_label);
 
     main_box.add(&toolbar_box);
     main_box.add(&content_box);
