5fa2e57aeb2c61e0a6186cb7cbe28c18c4e015b5
Change-Goose-Open-Surce-Software <dimitri.andreas.hiebert@gmail.com>
2026-01-10 11:47:02 +0100

    Fix type parameters for ScrolledWindow instantiation

--- main.rs
+++ main.rs
@@ -446,7 +446,7 @@ fn show_manual_window(
     sidebar.pack_start(&troubleshooting_btn, false, false, 0);
 
     // Content Area - KORRIGIERTE ZEILE
-    let scroll = ScrolledWindow::new(None, None);
+    let scroll = ScrolledWindow::new::<gtk::Adjustment, gtk::Adjustment>(None, None);
     let content_label = Label::new(None);
     content_label.set_line_wrap(true);
     content_label.set_margin_start(20);
